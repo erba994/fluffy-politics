35f07f06bebbef6f6dab55018189957878b12756
admin <ltosi@edu.hse.ru>
2021-10-12 15:46:28 +0200

    edit files

--- lib/fbcrawler/fbcrawl/settings.py
+++ lib/fbcrawler/fbcrawl/settings.py
@@ -15,7 +15,7 @@ SPIDER_MODULES = ['fbcrawl.spiders']
 NEWSPIDER_MODULE = 'fbcrawl.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (BB10; Kbd) AppleWebKit/537.35+ (KHTML, like Gecko) Version/10.3.3.2205 Mobile Safari/537.35+''
+USER_AGENT = 'Mozilla/5.0 (BB10; Kbd) AppleWebKit/537.35+ (KHTML, like Gecko) Version/10.3.3.2205 Mobile Safari/537.35+'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
